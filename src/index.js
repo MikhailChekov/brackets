33a7c7598f2b192fb740c98b63fde5ac4ad6dfe9
Mikhail Chekov <chekoff@list.ru>
2021-03-07 18:05:39 +0300

    feat: implement check method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let configs = bracketsConfig.map((e) => {
+    return e.join('');
+  });
+
+  // recursive method
+  function checkStr(str) {
+    let exit = true;
+
+    if (configs.some(e => {
+      if (str.indexOf(e) != -1) {
+        console.log('ok', e);
+        return true;
+      }
+
+    })) {
+      exit = false;
+    }
+
+    for (let i = 0; i < configs.length; i++) {
+      let config = configs[i];
+      if (str.indexOf(config) != -1) {
+        str = str.replace(config, '');
+      }
+    }
+    if (!exit && str) {
+      return checkStr(str);
+    } else {
+      return str;
+    }
+  }
+  str = checkStr(str);
+ 
+  return str.length === 0;
 }
